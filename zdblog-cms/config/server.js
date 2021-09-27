469b6f49ec7e2bc98a02cfded0474670d25704cc
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-09-27 21:24:10 +0100

    Initial Commit
    Create Strapi backend and Gatsby frontend with some initial code.

---
+++ zdblog-cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9703a6673423d64ff6a1e346959419ce'),
+    },
+  },
+});
---
+++ zdblog-cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6d1fa660-1060-4dc3-a400-9dd299dd5575'
+};
