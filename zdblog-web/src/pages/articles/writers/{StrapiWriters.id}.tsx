ac7fd86c10fadac9891a1c44989132662f1081bb
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-11-21 21:43:33 +0000

    Add individual writer

---
+++ zdblog-web/src/pages/articles/writers/{StrapiWriters.id}.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { graphql } from "gatsby";
+
+export const query = graphql`
+  query WriterQuery($id: String!) {
+    strapiWriters(id: { eq: $id }) {
+      email
+      name
+    }
+  }
+`;
+
+const writerPage = ({ data }) => {
+  return (
+    <div>
+      <h1>Work in Progress</h1>
+      <p>Category page</p>
+    </div>
+  );
+};
+
+export default writerPage;
