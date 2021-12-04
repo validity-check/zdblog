25ea978b38c4fd135cf82dd447c6eed21afdfe95
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-12-04 21:38:38 +0000

    Add a button and a container

--- zdblog-web/src/components/navbar.tsx
+++ zdblog-web/src/components/navbar.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { Link } from "gatsby";
 
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
 export const NavBar = () => {
   return (
     <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h4" noWrap>
-          ZDBlog
-        </Typography>
-      </Toolbar>
+      <Container maxWidth="xl">
+        <Toolbar>
+          <Typography variant="h4" noWrap>
+            ZDBlog
+          </Typography>
+          <Button sx={{ my: 2, color: "white", display: "block" }}>Test</Button>
+        </Toolbar>
+      </Container>
     </AppBar>
   );
 };
