6203f8563f7f38f82306cc81a8dcecb03ae39b83
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-12-17 21:43:39 +0000

    Make it dynamic

--- zdblog-web/src/pages/articles.tsx
+++ zdblog-web/src/pages/articles.tsx
@@ -1,60 +1,55 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 import Grid from "@mui/material/Grid";
 
 import { ArticleListItem } from "../components/article-list-item";
 
-const query = graphql`
-  {
-    allStrapiArticles {
-      edges {
-        node {
-          description
-          title
-          createdAt
-          slug
-          author {
-            name
-            picture {
+// markup
+const ArticlesPage = () => {
+  const data = useStaticQuery(graphql`
+    query ArticlesQuery {
+      allStrapiArticles {
+        edges {
+          node {
+            id
+            description
+            title
+            createdAt
+            slug
+            author {
+              name
+              picture {
+                url
+                id
+              }
+            }
+            image {
               url
             }
           }
         }
       }
     }
-  }
-`;
+  `);
 
-// markup
-const ArticlesPage = () => {
   return (
     <div>
       <Grid container spacing={2}>
-        <Grid item>
-          <ArticleListItem
-            title="hello card"
-            description="This card says hello"
-            writer="r"
-            writerImage="https://picsum.photos/200"
-            writerId="writers-61504e7a8dd36c518cb2c952"
-            slug="hello-card"
-            image="https://source.unsplash.com/random"
-            createdAt="2020-01-01"
-          ></ArticleListItem>
-        </Grid>
-        <Grid item>
-          <ArticleListItem
-            title="helloo card"
-            description="This card also says hello!"
-            writer="h"
-            writerId=""
-            writerImage="https://picsum.photos/200"
-            slug="helloo-card"
-            image="https://source.unsplash.com/random"
-            createdAt="2020-07-23"
-          ></ArticleListItem>
-        </Grid>
+        {data.allStrapiArticles.edges.map(({ node }) => (
+          <Grid item xs={12} sm={6} md={4} key={node.id}>
+            <ArticleListItem
+              title={node.title}
+              description={node.description}
+              createdAt={node.createdAt}
+              slug={node.slug}
+              writer={node.author.name}
+              writerImage={process.env.API_URL + node.author.picture.url}
+              writerId={node.author.picture.id}
+              image={process.env.API_URL + node.image.url}
+            />
+          </Grid>
+        ))}
       </Grid>
     </div>
   );
