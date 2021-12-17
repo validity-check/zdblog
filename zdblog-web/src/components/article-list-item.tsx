70cbebae4421bcaf3f190165a99bc813ebeb9712
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-12-17 20:23:10 +0000

    Add typing

--- zdblog-web/src/components/article-list-item.tsx
+++ zdblog-web/src/components/article-list-item.tsx
@@ -1,14 +1,28 @@
 import * as React from "react";
 
-import Card from "@mui/material/card";
-import CardHeader from "@mui/material/CardHeader";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 
-export const ArticleListItem = (props) => {
+type Props = {
+  title: string;
+  description: string;
+  author: string;
+  slug: string;
+  image: string;
+};
+
+export const ArticleListItem = ({
+  title,
+  description,
+  author,
+  slug,
+  image,
+}: Props) => {
   return (
-    <Card>
-      <CardHeader>{props.title}</CardHeader>
-      <CardMedia component="img" height="194" image={props.image} />
+    <Card sx={{ maxWidth: 345, m: 5 }}>
+      <CardMedia component="img" height="194" image={image} />
+      <CardContent></CardContent>
     </Card>
   );
 };
