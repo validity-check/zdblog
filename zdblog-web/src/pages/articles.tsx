d314def8d8a8e8153efc0e61c2d6716b2e1b74fd
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-12-17 20:49:49 +0000

    Implement article list item in articles file

--- zdblog-web/src/pages/articles.tsx
+++ zdblog-web/src/pages/articles.tsx
@@ -1,23 +1,41 @@
-import * as React from 'react';
+import * as React from "react";
 
-import Grid from '@mui/material/Grid';
+import Grid from "@mui/material/Grid";
 
 import { ArticleListItem } from "../components/article-list-item";
 
 // markup
 const ArticlesPage = () => {
-    return (
-        <div>
-            <Grid container spacing={2}>
-                <Grid item xs={8}>
-                    <ArticleListItem title="hello card"></ArticleListItem>
-                </Grid>
-                <Grid item xs={8}>
-                    <ArticleListItem title="helloo card"></ArticleListItem>
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Grid container spacing={2}>
+        <Grid item>
+          <ArticleListItem
+            title="hello card"
+            description="This card says hello"
+            writer="r"
+            writerImage="https://picsum.photos/200"
+            writerId="writers-61504e7a8dd36c518cb2c952"
+            slug="hello-card"
+            image="https://source.unsplash.com/random"
+            createdAt="2020-01-01"
+          ></ArticleListItem>
+        </Grid>
+        <Grid item>
+          <ArticleListItem
+            title="helloo card"
+            description="This card also says hello!"
+            writer="h"
+            writerId=""
+            writerImage="https://picsum.photos/200"
+            slug="helloo-card"
+            image="https://source.unsplash.com/random"
+            createdAt="2020-07-23"
+          ></ArticleListItem>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default ArticlesPage;
