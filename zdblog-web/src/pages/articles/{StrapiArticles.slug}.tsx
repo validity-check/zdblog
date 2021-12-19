6a1d8bc72df855a768a3e9947d704b8208252e9b
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2021-12-19 21:58:32 +0000

    Write GraphQL request

--- zdblog-web/src/pages/articles/{StrapiArticles.slug}.tsx
+++ zdblog-web/src/pages/articles/{StrapiArticles.slug}.tsx
@@ -2,14 +2,25 @@ import React from "react";
 
 import { graphql } from "gatsby";
 
+import Typography from "@mui/material/Typography";
+
 import { Layout } from "../../components/layout";
 
 export const query = graphql`
   query ArticleQuery($slug: String!) {
     strapiArticles(slug: { eq: $slug }) {
+      title
       description
       content
-      published_at
+      published_at(formatString: "DD/MM/YY")
+      author {
+        name
+        picture {
+          localFile {
+            url
+          }
+        }
+      }
     }
   }
 `;
@@ -17,8 +28,8 @@ export const query = graphql`
 const articlePage = ({ data }) => {
   return (
     <Layout>
-      <h1>Article</h1>
-      <p>Work in Progress</p>
+      {/* <img src={data.}></img> */}
+      <Typography variant="h1">{data.strapiArticles.title}</Typography>
     </Layout>
   );
 };
