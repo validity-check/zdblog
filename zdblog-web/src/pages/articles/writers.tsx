77972b6912c8a58b6c4d9586a779d42bf4203d2a
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2022-01-01 19:11:58 +0000

    Begin Card

--- zdblog-web/src/pages/articles/writers.tsx
+++ zdblog-web/src/pages/articles/writers.tsx
@@ -1,10 +1,15 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
 
 import { Layout } from "../../components/layout/layout";
 
-const writersPage = ({ data }) => {
-  const query = graphql`
+const writersPage = () => {
+  const data = useStaticQuery(graphql`
     query WritersQuery {
       allStrapiWriters {
         edges {
@@ -22,12 +27,25 @@ const writersPage = ({ data }) => {
         }
       }
     }
-  `;
+  `);
 
   return (
     <Layout>
-      <h1>Writer list</h1>
-      <p>Work in Progress</p>
+      <Grid container spacing={2}>
+        {data.allStrapiWriters.edges.map(({ node }) => (
+          <Grid item xs={12} sm={6} md={4} key={node.id}>
+            <Card>
+              <CardMedia>
+                <GatsbyImage
+                  image={getImage(node.picture.localFile.childImageSharp)}
+                  alt={node.name}
+                />
+              </CardMedia>
+              <h3>{node.name}</h3>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </Layout>
   );
 };
