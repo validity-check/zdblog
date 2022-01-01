b835bdd1243d011880085990d663b23955c1af8f
Zaid Dar <63739210+validity-check@users.noreply.github.com>
2022-01-01 22:52:18 +0000

    Wrap in CardActionArea Link leads to writer's page (to be completed as of yet)

--- zdblog-web/src/pages/articles/writers.tsx
+++ zdblog-web/src/pages/articles/writers.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import { useStaticQuery, graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
@@ -7,6 +8,7 @@ import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import CardActionArea from "@mui/material/CardActionArea";
 
 import { Layout } from "../../components/layout/layout";
 
@@ -16,7 +18,7 @@ const writersPage = () => {
       allStrapiWriters {
         edges {
           node {
-            id
+            strapiId
             name
             picture {
               localFile {
@@ -35,17 +37,28 @@ const writersPage = () => {
     <Layout>
       <Grid container spacing={2} sx={{ padding: 5 }}>
         {data.allStrapiWriters.edges.map(({ node }) => (
-          <Grid item xs={12} sm={6} md={4} key={node.id}>
+          <Grid item xs={12} sm={6} md={4} key={node.strapiId}>
             <Card>
-              <CardMedia>
-                <GatsbyImage
-                  image={getImage(node.picture.localFile.childImageSharp)}
-                  alt={node.name}
-                />
-              </CardMedia>
-              <CardContent>
-                <Typography variant="h6">{node.name}</Typography>
-              </CardContent>
+              <CardActionArea
+                to={`writers-${node.strapiId}`}
+                component={Link}
+                sx={{
+                  textDecoration: "none",
+                  "&:hover": {
+                    textDecoration: "none",
+                  },
+                }}
+              >
+                <CardMedia>
+                  <GatsbyImage
+                    image={getImage(node.picture.localFile.childImageSharp)}
+                    alt={node.name}
+                  />
+                </CardMedia>
+                <CardContent>
+                  <Typography variant="h6">{node.name}</Typography>
+                </CardContent>
+              </CardActionArea>
             </Card>
           </Grid>
         ))}
